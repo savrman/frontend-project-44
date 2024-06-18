a2b2eb48b80faefcfcc02eb5588fdace7cee0d54
Savr Mandzhiev <163935933+savrman@users.noreply.github.com>
2024-06-18 03:17:49 +0300

    Fix linter errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import introduce from '../src/cli.js'
-console.log('Welcome to the Brain Games!');
-introduce();
+import cli from '../src/cli.js';
 
+console.log('Welcome to the Brain Games!');
+cli();
--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 const introduce = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
-}
+};
 
 export default introduce;
