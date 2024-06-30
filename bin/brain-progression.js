93406994270fdcf9b45954e8035402c68345ec93
Savr Mandzhiev <163935933+savrman@users.noreply.github.com>
2024-06-30 17:34:48 +0300

    Add game Progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import { playGame } from "../src/index.js";
+import { gameDescription, getGameSet } from "../src/games/brain-progression.js";
+
+playGame(gameDescription, getGameSet);
